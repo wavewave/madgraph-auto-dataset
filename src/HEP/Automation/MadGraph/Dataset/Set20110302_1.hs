0f88013910b8f7d7364dc5884152ea14d3961ee1
ianwookim <ianwookim@gmail.com>
2011-03-02 16:07:35 +0000

    setup Set20110302_1

--- src/HEP/Automation/MadGraph/Dataset/Set20110302_1.hs
+++ src/HEP/Automation/MadGraph/Dataset/Set20110302_1.hs
@@ -1,15 +1,13 @@
 {-# LANGUAGE PackageImports #-}
 
-module HEP.Automation.MadGraph.Dataset.Set20110302 where
+module HEP.Automation.MadGraph.Dataset.Set20110302_1 where
 
-import Control.Monad.Reader 
 
 import HEP.Automation.MadGraph.Model
 import HEP.Automation.MadGraph.Machine
 import HEP.Automation.MadGraph.UserCut
 import HEP.Automation.MadGraph.Cluster
 import HEP.Automation.MadGraph.SetupType
-import HEP.Automation.MadGraph.Run
 
 my_ssetup :: ScriptSetup
 my_ssetup = SS {
@@ -31,19 +29,19 @@ processTTBar0or1jet :: [Char]
 processTTBar0or1jet =  
   "\ngenerate P P > t t~  QED=99 @1 \nadd process P P > t t~ J QED=99 @2 \n"
 
-psetup_zp_ttbar01j :: ProcessSetup
-psetup_zp_ttbar01j = PS {  
+psetup_wp_ttbar01j :: ProcessSetup
+psetup_wp_ttbar01j = PS {  
     mversion = MadGraph4
-  , model = ZpH 
+  , model = Wp
   , process = processTTBar0or1jet 
   , processBrief = "ttbar01j"  
-  , workname   = "228ZpH1J"
+  , workname   = "302Wp1J"
   }
 
 rsetupGen :: Param -> MatchType -> Int -> RunSetup
 rsetupGen p matchtype num = RS { 
     param   = p
-  , numevent = 20000
+  , numevent = 100000
   , machine = TeVatron 
   , rgrun   = Fixed
   , rgscale = 200.0 
@@ -54,32 +52,30 @@ rsetupGen p matchtype num = RS {
   , pythia  = case matchtype of 
       NoMatch -> NoPYTHIA
       MLM     -> RunPYTHIA
-  , usercut = NoUserCutDef -- UserCutDef ucut 
-  , pgs     = NoPGS -- RunPGS
+  , usercut = UserCutDef ucut 
+  , pgs     = RunPGS
   , setnum  = num
 }
 
 my_csetup :: ClusterSetup
-my_csetup = CS { cluster = Parallel 3 
- -- Cluster "test" 
-}
+my_csetup = CS { cluster = Parallel 3 }
 
-zpparamset :: [Param]
-zpparamset = [ ZpHParam m x | x <- [0.6, 0.8 .. 4.0 ], m <- [200.0, 300.0 .. 800.0]  ]
+wpparamset :: [Param]
+wpparamset = [ WpParam 400.0 2.55 ]
 
 psetuplist :: [ProcessSetup]
-psetuplist = [ psetup_zp_ttbar01j ]
+psetuplist = [ psetup_wp_ttbar01j ]
 
 sets :: [Int]
-sets = [1 ]
+sets = [1 .. 50 ]
 
-zptasklist :: [WorkSetup]
-zptasklist =  [ WS my_ssetup (psetup_zp_ttbar01j) (rsetupGen p MLM num) my_csetup  
-                | p <- zpparamset 
+wptasklist :: [WorkSetup]
+wptasklist =  [ WS my_ssetup (psetup_wp_ttbar01j) (rsetupGen p MLM num) my_csetup  
+                | p <- wpparamset 
                 , num <- sets     ]
 
 totaltasklist :: [WorkSetup]
-totaltasklist = zptasklist 
+totaltasklist = wptasklist 
 
 
 
