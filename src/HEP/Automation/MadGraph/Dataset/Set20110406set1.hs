606605fdb64a0f47d704918f098ac7351ef43394
ianwookim <ianwookim@gmail.com>
2011-04-07 03:06:52 +0000

    bug fix in 20110406set1

---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110406set1.hs
@@ -0,0 +1,58 @@
+module HEP.Automation.MadGraph.Dataset.Set20110406set1 where
+
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.Cluster
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Dataset.Common
+import HEP.Automation.MadGraph.Dataset.Desktop
+
+import HEP.Automation.MadGraph.Model.ZpHFull
+
+import qualified Data.ByteString as B
+
+processSemiZp :: [Char]
+processSemiZp =  
+  "\ngenerate P P > t t~, t > b w+, (t~ > u~ zput, zput > b~ d) @1 \nadd process P P > t t~, (t > u zptu, zptu > b d~), t~ > b~ w- @2\n"
+
+psetup_zphfull_semizp :: ProcessSetup ZpHFull
+psetup_zphfull_semizp = PS {  
+    mversion = MadGraph4
+  , model = ZpHFull
+  , process = processSemiZp
+  , processBrief = "semizp"  
+  , workname   = "406ZpH_SemiZp"
+  }
+
+my_csetup :: ClusterSetup
+my_csetup = CS { cluster = Parallel 6 }
+
+zpHFullParamSet :: [ModelParam ZpHFull]
+zpHFullParamSet = [ ZpHFullParam m g g 
+                    | m <-[150.0] 
+                    , g <- [0.5*sqrt 2] ] 
+          
+
+psetuplist :: [ProcessSetup ZpHFull]
+psetuplist = [ psetup_zphfull_semizp ]
+
+sets :: [Int]
+sets = [1]
+
+zptasklist :: [WorkSetup ZpHFull]
+zptasklist =  [ WS my_ssetup (psetup_zphfull_semizp) 
+                     (rsetupGen p NoMatch NoUserCutDef NoPGS 20000 num) 
+                     my_csetup  
+                 | p <- zpHFullParamSet , num <- sets     ]
+
+
+
+totaltasklist :: [WorkSetup ZpHFull]
+totaltasklist = zptasklist 
+
+
+
+          
