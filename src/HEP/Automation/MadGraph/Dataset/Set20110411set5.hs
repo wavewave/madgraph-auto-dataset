64f6d6fd02c6bcf7b597ac7d90618955392d5de2
ianwookim <ianwookim@gmail.com>
2011-04-12 05:18:15 +0000

    add Set20110411set5.hs

---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110411set5.hs
@@ -0,0 +1,51 @@
+module HEP.Automation.MadGraph.Dataset.Set20110411set5 where
+
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.Cluster
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Model.ZpH
+
+import HEP.Automation.MadGraph.Dataset.Common
+
+
+processSingleTZpSemiLep :: [Char]
+processSingleTZpSemiLep =  
+  "\ngenerate    P P > t zput  QED=99, (zput > t~ u, (t~ > b~ w-, w- > L- vl~ )), (t > b w+ , w+ > J J ) @1 \nadd process P P > t zput  QED=99, (zput > t~ u, (t~ > b~ w-, w- > J J    )), (t > b w+ , w+ > L+ vl ) @2 \nadd process P P > zptu t~ QED=99, (zptu > t u~, (t > b w+, w+ > L+ vl    )), (t~ > b~ w-, w- > J J ) @3 \nadd process P P > zptu t~ QED=99, (zptu > t u~, (t > b w+, w+ > J J      )), (t~ > b~ w-, w- > L- vl~ ) @4 \n"
+
+psetup_zp_SingleTZpSemiLep :: ProcessSetup ZpH
+psetup_zp_SingleTZpSemiLep = PS {  
+    mversion = MadGraph4
+  , model = ZpH
+  , process = processSingleTZpSemiLep
+  , processBrief = "SingleTZpSemiLep"  
+  , workname   = "412_ZpH_SingleTZpSemiLep"
+  }
+
+zpparamset :: [ModelParam ZpH]
+zpparamset = [ ZpHParam mass g | mass <- [400.0] , g <- [1.75] ] 
+          
+
+psetuplist :: [ProcessSetup ZpH]
+psetuplist = [ psetup_zp_SingleTZpSemiLep ]
+
+sets :: [Int]
+sets = [101,102,201,202,301,302,401,402,501,502,601,602,701,702,801,802,901,902,1001,1002]
+
+zptasklist :: ScriptSetup -> ClusterSetup -> [WorkSetup ZpH]
+zptasklist ssetup csetup =  
+  [ WS ssetup (psetup_zp_SingleTZpSemiLep) 
+       (rsetupLHC p NoMatch  NoUserCutDef RunPGS 10000 num) 
+       csetup  
+  | p <- zpparamset 
+  , num <- sets     ]
+
+totaltasklist :: ScriptSetup -> ClusterSetup -> [WorkSetup ZpH]
+totaltasklist = zptasklist 
+
+
+
+          
