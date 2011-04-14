eba02adf58642233fb9e4d82e4bb0116b4558eea
ianwookim <ianwookim@gmail.com>
2011-04-14 00:43:07 +0000

    add Set20110413set2

---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110412set4.hs
@@ -0,0 +1,56 @@
+module HEP.Automation.MadGraph.Dataset.Set20110412set4 where
+
+import HEP.Storage.WebDAV
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.Cluster
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Model.Six
+
+import HEP.Automation.MadGraph.Dataset.Common
+
+import qualified Data.ByteString as B
+
+processTTBar :: [Char]
+processTTBar =  
+  "\ngenerate P P > t t~ QED=99\n"
+
+psetup_six_ttbar :: ProcessSetup Six
+psetup_six_ttbar = PS {  
+    mversion = MadGraph5
+  , model = Six
+  , process = processTTBar
+  , processBrief = "ttbar" 
+  , workname   = "412Six_TTBar"
+  }
+
+sixParamSet :: [ModelParam Six]
+sixParamSet = [ SixParam m g  
+                    | m <-[135.0, 140.0 .. 155.0] 
+                    , g <- [0.20, 0.25 .. 1.40] ] 
+          
+
+psetuplist :: [ProcessSetup Six]
+psetuplist = [ psetup_six_ttbar ]
+
+sets :: [Int]
+sets = [1]
+
+sixtasklist :: ScriptSetup -> ClusterSetup -> [WorkSetup Six]
+sixtasklist ssetup csetup =  
+  [ WS ssetup (psetup_six_ttbar) 
+       (rsetupGen p NoMatch NoUserCutDef NoPGS 10000 num) 
+       csetup  
+       (WebDAVRemoteDir "mc/TeVatronFor3/Six0412ScanTTBar")
+  | p <- sixParamSet , num <- sets     ]
+
+
+totaltasklist :: ScriptSetup -> ClusterSetup -> [WorkSetup Six]
+totaltasklist = sixtasklist 
+
+
+
+          
---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110413set2.hs
@@ -0,0 +1,55 @@
+module HEP.Automation.MadGraph.Dataset.Set20110413set2 where
+
+import HEP.Storage.WebDAV
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.Cluster
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Model.AxiGluon
+
+import HEP.Automation.MadGraph.Dataset.Common
+
+import qualified Data.ByteString as B
+
+processTTBar :: [Char]
+processTTBar =  
+  "\ngenerate P P > t t~ QED=99\n"
+
+psetup_axi_ttbar :: ProcessSetup AxiGluon
+psetup_axi_ttbar = PS {  
+    mversion = MadGraph4
+  , model = AxiGluon
+  , process = processTTBar
+  , processBrief = "ttbar" 
+  , workname   = "413AxiGluon_TTBar"
+  }
+
+axiParamSet :: [ModelParam AxiGluon]
+axiParamSet = [ AxiGluonParam m 0.0 0.0 g1 g2  
+                    | m <-[150.0] 
+                    , g1 <- [0.10, 0.20 .. 2.00] 
+                    , g2 <- [0.10, 0.20 .. 2.00] ] 
+          
+
+psetuplist :: [ProcessSetup AxiGluon]
+psetuplist = [ psetup_axi_ttbar ]
+
+sets :: [Int]
+sets = [1]
+
+axitasklist :: ScriptSetup -> ClusterSetup -> [WorkSetup AxiGluon]
+axitasklist ssetup csetup =  
+  [ WS ssetup (psetup_axi_ttbar) 
+       (rsetupGen p NoMatch NoUserCutDef NoPGS 10000 num) 
+       csetup  
+       (WebDAVRemoteDir "mc/TeVatronFor3/AxiGluon0413ScanTTBar")
+  | p <- axiParamSet , num <- sets     ]
+
+
+totaltasklist :: ScriptSetup -> ClusterSetup -> [WorkSetup AxiGluon]
+totaltasklist = axitasklist 
+
+          
