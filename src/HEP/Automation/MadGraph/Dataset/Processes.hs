04ef2c9f6c84a38832833f489ceeca6b35810f02
ianwookim <ianwookim@gmail.com>
2011-04-19 20:41:57 +0000

    fix parenthesis bug in Processes.hs

--- src/HEP/Automation/MadGraph/Dataset/Processes.hs
+++ src/HEP/Automation/MadGraph/Dataset/Processes.hs
@@ -11,7 +11,7 @@ preDefProcess SingleTZpSemiLep = "\ngenerate    P P > t zput  QED=99, (t > b w+ 
 preDefProcess TTBarSemiZp = "\ngenerate P P > t t~ QED=99, ( t > b w+, w+ > l+ vl ), (t~ > u~ zput, zput > b~ d ) @1 \nadd process P P > t t~ QED=99, (t > u zptu, zptu > b d~ ), (t~ > b~ w-, w- > l- vl~ ) @2 \n"
 preDefProcess TZpLep      = "\ngenerate P P > t zput QED=99, zput > b~ d, ( t > b w+, w+ > l+ vl ) @1 \nadd process P P > t~ zptu QED=99, (t~ > b~ w-, w- > l- vl~ ), zptu > b d~ @2 \n"
 preDefProcess TTBarSemiZpNotFull = "\ngenerate P P > t t~ QED=99, t > b w+, t~ > u~ zput @1 \nadd process P P > t t~ QED=99, t > u zptu, t~ > b~ w- @2 \n"
-preDefProcess TTBarSemiLep = "\ngenerate    P P > t t~  QED=99, (t > b w+ , w+ > J J ), (t~ > b~ w-, w- > L- vl~ )) @1\nadd process P P > t zput  QED=99, (t > b w+ , w+ > L+ vl ), (t~ > b~ w-, w- > J J    ) @2\nadd process P P > t t~ QED=99, (t > b w+, w+ > L+ vl    ), (t~ > b~ w-, w- > J J ) @3\nadd process P P > t t~ QED=99, (t > b w+, w+ > J J      ), (t~ > b~ w-, w- > L- vl~ ) @4\n"
+preDefProcess TTBarSemiLep = "\ngenerate    P P > t t~  QED=99, (t > b w+ , w+ > J J ), (t~ > b~ w-, w- > L- vl~ ) @1\nadd process P P > t zput  QED=99, (t > b w+ , w+ > L+ vl ), (t~ > b~ w-, w- > J J    ) @2\nadd process P P > t t~ QED=99, (t > b w+, w+ > L+ vl    ), (t~ > b~ w-, w- > J J ) @3\nadd process P P > t t~ QED=99, (t > b w+, w+ > J J      ), (t~ > b~ w-, w- > L- vl~ ) @4\n"
 
 
 
