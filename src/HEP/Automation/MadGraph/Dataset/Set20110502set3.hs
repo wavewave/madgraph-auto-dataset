ea3eb17e0ba0136dcc5a909250ab2a1259a0550c
ianwookim <ianwookim@gmail.com>
2011-05-03 19:50:05 +0000

    missing Set20110502set3.hs

---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110502set3.hs
@@ -0,0 +1,64 @@
+module HEP.Automation.MadGraph.Dataset.Set20110502set3 where
+
+import HEP.Storage.WebDAV
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Model.ZpHFull
+
+import HEP.Automation.MadGraph.Dataset.Common
+import HEP.Automation.MadGraph.Dataset.Processes
+
+
+import qualified Data.ByteString as B
+
+psetup_zphfull_TZpLep :: ProcessSetup ZpHFull
+psetup_zphfull_TZpLep = PS {  
+    mversion = MadGraph4
+  , model = ZpHFull
+  , process = preDefProcess TZpLep
+  , processBrief = "TZpLep" 
+  , workname   = "428ZpH_TZpLep"
+  }
+
+zpHFullParamSet :: [ModelParam ZpHFull]
+zpHFullParamSet = [ ZpHFullParam m g (0.28) 
+                    | m <-[145.0]
+                    , g <- [0.90] ]
+          
+
+psetuplist :: [ProcessSetup ZpHFull]
+psetuplist = [ psetup_zphfull_TZpLep ]
+
+sets :: [Int]
+sets = [1,2..20] -- [102,103..110] -- [101]
+
+zptasklist :: ScriptSetup -> ClusterSetup ZpHFull -> [WorkSetup ZpHFull]
+zptasklist ssetup csetup =  
+  [ WS ssetup (psetup_zphfull_TZpLep) 
+       (RS { param = p
+           , numevent = 10000
+           , machine = TeVatron 
+           , rgrun   = Fixed
+           , rgscale = 200.0
+           , match   = NoMatch
+           , cut     = DefCut 
+           , pythia  = RunPYTHIA
+           , usercut = NoUserCutDef
+           , pgs     = RunPGS 
+           , setnum  = num 
+           })
+       csetup  
+       (WebDAVRemoteDir "mc/TeVatronFor3/ZpHFull0502Big")
+  | p <- zpHFullParamSet , num <- sets     ]
+
+
+totaltasklistEvery :: Int -> Int -> ScriptSetup -> ClusterSetup ZpHFull -> [WorkSetup ZpHFull]
+totaltasklistEvery n r ssetup csetup = 
+  let lst = zip [1..] (zptasklist ssetup csetup)
+  in  map snd . filter (\(x,_)-> x `mod` n == r) $ lst 
+
+
