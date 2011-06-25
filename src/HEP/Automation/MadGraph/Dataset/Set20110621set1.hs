4f760e903bfea67807eab57f7a5642cc46274bbf
ianwookim <ianwookim@gmail.com>
2011-06-25 13:56:14 +0000

    add Set20110621set1.hs and comment out others from cabal

---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110621set1.hs
@@ -0,0 +1,59 @@
+module HEP.Automation.MadGraph.Dataset.Set20110621set1 where
+
+import HEP.Storage.WebDAV
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Model.AxiGluon
+
+import HEP.Automation.MadGraph.Dataset.Common
+import HEP.Automation.MadGraph.Dataset.Processes
+
+import HEP.Automation.JobQueue.JobType
+
+import qualified Data.ByteString as B
+
+psetup_axi_WGDijet :: ProcessSetup AxiGluon
+psetup_axi_WGDijet = PS {  
+    mversion = MadGraph4
+  , model = AxiGluon
+  , process = preDefProcess WGDijet
+  , processBrief = "WGDijet" 
+  , workname   = "621Axi_WGDijet"
+  }
+
+axiParamSet :: [ModelParam AxiGluon]
+axiParamSet = [ AxiGluonParam { massAxiG = 140.0,  
+                                gVq = 0.0,  
+                                gVt = 0.0, 
+                                gAq = 0.3,  
+                                gAt = 1.2 } ]
+              
+
+psetuplist :: [ProcessSetup AxiGluon ]
+psetuplist = [ psetup_axi_WGDijet ]
+
+sets :: [Int]
+sets = [1,2..80]
+
+eventsets :: [EventSet]
+eventsets =  
+  [ EventSet  (psetup_axi_WGDijet) 
+              (RS { param = p
+                  , numevent = 10000
+                  , machine = TeVatron 
+                  , rgrun   = Fixed
+                  , rgscale = 200.0
+                  , match   = NoMatch
+                  , cut     = DefCut 
+                  , pythia  = RunPYTHIA
+                  , usercut = NoUserCutDef
+                  , pgs     = RunPGSNoTau
+                  , setnum  = num 
+                  })
+   | p <- axiParamSet , num <- sets     ]
+
+
