18132c6387952ac9242481c9c59b3e5fe2858931
ianwookim <ianwookim@gmail.com>
2011-07-07 01:45:49 +0000

    add Set20110706set2.hs

---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110706set1.hs
@@ -0,0 +1,66 @@
+module HEP.Automation.MadGraph.Dataset.Set20110706set1 where
+
+import HEP.Storage.WebDAV.Type
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Model.ZpH
+
+import HEP.Automation.MadGraph.Dataset.Processes
+
+import HEP.Automation.JobQueue.JobType
+
+psetup_zph_TTBar0or1J :: ProcessSetup ZpH
+psetup_zph_TTBar0or1J = PS {  
+    mversion = MadGraph4
+  , model = ZpH
+  , process = preDefProcess TTBar0or1J
+  , processBrief = "TTBar0or1J" 
+  , workname   = "628ZpH_TTBar0or1J"
+  }
+
+zphParamSet :: [ModelParam ZpH]
+zphParamSet = [ ZpHParam { massZp = 800.0,  
+                           gRZp = 3.4 } ]
+              
+
+psetuplist :: [ProcessSetup ZpH ]
+psetuplist = [ psetup_zph_TTBar0or1J ]
+
+sets :: [Int]
+sets =  [1..50]
+
+ucut :: UserCut 
+ucut = UserCut { 
+    uc_metcut = 15.0
+  , uc_etacutlep = 2.7
+  , uc_etcutlep = 18.0 
+  , uc_etacutjet = 2.7
+  , uc_etcutjet = 15.0 
+}
+
+
+eventsets :: [EventSet]
+eventsets =  
+  [ EventSet  (psetup_zph_TTBar0or1J) 
+              (RS { param = p
+                  , numevent = 100000
+                  , machine = LHC7 ATLAS 
+                  , rgrun   = Fixed
+                  , rgscale = 200.0
+                  , match   = MLM
+                  , cut     = DefCut 
+                  , pythia  = RunPYTHIA
+                  , usercut = UserCutDef ucut
+                  , pgs     = RunPGS
+                  , jetalgo = AntiKTJet 0.4
+                  , uploadhep = NoUploadHEP
+                  , setnum  = num 
+                  })
+   | p <- zphParamSet , num <- sets     ]
+
+webdavdir :: WebDAVRemoteDir
+webdavdir = WebDAVRemoteDir "paper3/ttbarLHC"
