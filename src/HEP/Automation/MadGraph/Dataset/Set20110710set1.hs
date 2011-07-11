455abdf536d2d3f0291202e6eb9f4dc43caa2b71
ianwookim <ianwookim@gmail.com>
2011-07-11 02:41:44 +0000

    add Set20110710set1.hs

---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110710set1.hs
@@ -0,0 +1,68 @@
+module HEP.Automation.MadGraph.Dataset.Set20110710set1 where
+
+import HEP.Storage.WebDAV.Type
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Model.ZpH
+
+import HEP.Automation.MadGraph.Dataset.Processes
+
+import HEP.Automation.JobQueue.JobType
+
+psetup_zph_TTBar0or1J :: ProcessSetup ZpH
+psetup_zph_TTBar0or1J = PS {  
+    model = ZpH
+  , process = preDefProcess TTBar0or1J
+  , processBrief = "TTBar0or1J" 
+  , workname   = "707_ZpH_TTBar0or1J_TEV"
+  }
+
+zphParamSet :: [ModelParam ZpH]
+zphParamSet = [ ZpHParam { massZp = m,  
+                           gRZp = g } | m <- [600,800,1000], g <- [1,3,5] ]
+-- m <- [200,400,600,800,1000],  g <- [1,2,3,4,5] ]  
+-- m <- [200,300..1000],
+--  g <- [1,2,3,4,5] ]
+              
+
+psetuplist :: [ProcessSetup ZpH ]
+psetuplist = [ psetup_zph_TTBar0or1J ]
+
+sets :: [Int]
+sets =  [1]
+
+ucut :: UserCut 
+ucut = UserCut { 
+    uc_metcut = 15.0
+  , uc_etacutlep = 2.7
+  , uc_etcutlep = 18.0 
+  , uc_etacutjet = 2.7
+  , uc_etcutjet = 15.0 
+}
+
+
+eventsets :: [EventSet]
+eventsets =  
+  [ EventSet  (psetup_zph_TTBar0or1J) 
+              (RS { param = p
+                  , numevent = 100000
+                  , machine = TeVatron 
+                  , rgrun   = Fixed
+                  , rgscale = 200.0
+                  , match   = MLM
+                  , cut     = DefCut 
+                  , pythia  = RunPYTHIA
+                  , usercut = NoUserCutDef -- UserCutDef ucut
+                  , pgs     = RunPGS
+                  , jetalgo = Cone 0.4
+                  , uploadhep = NoUploadHEP
+                  , setnum  = num 
+                  })
+   | p <- zphParamSet , num <- sets     ]
+
+webdavdir :: WebDAVRemoteDir
+webdavdir = WebDAVRemoteDir "paper3/test"
