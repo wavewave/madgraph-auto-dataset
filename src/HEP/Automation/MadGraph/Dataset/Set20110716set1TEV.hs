81b89d4fa681aa63df7f748ca823a9f48cf9eb86
ianwookim <ianwookim@gmail.com>
2011-08-02 16:54:41 +0000

    add 20110716set1

---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110716set1TEV.hs
@@ -0,0 +1,61 @@
+module HEP.Automation.MadGraph.Dataset.Set20110716set1TEV where
+
+import HEP.Storage.WebDAV.Type
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Model.C1V
+
+import HEP.Automation.MadGraph.Dataset.Processes
+
+import HEP.Automation.JobQueue.JobType
+
+processSetup :: ProcessSetup C1V
+processSetup = PS {  
+    model = C1V
+  , process = preDefProcess TTBar0or1J
+  , processBrief = "TTBar0or1J" 
+  , workname   = "716_C1V_TTBar0or1J_TEV"
+  }
+
+paramSet :: [ModelParam C1V]
+paramSet = [ C1VParam { mnp = m, gnpR = g, gnpL = 0 } 
+           | (m,g) <- [(600,1.45), (600,1.5), (800,2.0), (800,1.9), (800,2.1) ] ] 
+
+sets :: [Int]
+sets =  [1..50]
+
+ucut :: UserCut 
+ucut = UserCut { 
+    uc_metcut = 15.0
+  , uc_etacutlep = 2.7
+  , uc_etcutlep = 18.0 
+  , uc_etacutjet = 2.7
+  , uc_etcutjet = 15.0 
+}
+
+
+eventsets :: [EventSet]
+eventsets =  
+  [ EventSet  processSetup 
+              (RS { param = p
+                  , numevent = 100000
+                  , machine = TeVatron
+                  , rgrun   = Fixed
+                  , rgscale = 200.0
+                  , match   = MLM
+                  , cut     = DefCut 
+                  , pythia  = RunPYTHIA
+                  , usercut = UserCutDef ucut --  NoUserCutDef -- 
+                  , pgs     = RunPGS
+                  , jetalgo = Cone 0.4
+                  , uploadhep = NoUploadHEP
+                  , setnum  = num 
+                  })
+   | p <- paramSet , num <- sets     ]
+
+webdavdir :: WebDAVRemoteDir
+webdavdir = WebDAVRemoteDir "paper3/ttbar_TEV_c1v_big"
