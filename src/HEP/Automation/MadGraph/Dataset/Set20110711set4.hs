b7c42cc83f3752732d404ecce71ef6d65e8db38a
ianwookim <ianwookim@gmail.com>
2011-08-02 16:50:31 +0000

    add missing Set20110711set4.hs

---
+++ src/HEP/Automation/MadGraph/Dataset/Set20110711set4.hs
@@ -0,0 +1,63 @@
+module HEP.Automation.MadGraph.Dataset.Set20110711set4 where
+
+import HEP.Storage.WebDAV.Type
+
+import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Machine
+import HEP.Automation.MadGraph.UserCut
+import HEP.Automation.MadGraph.SetupType
+
+import HEP.Automation.MadGraph.Model.C8V
+
+import HEP.Automation.MadGraph.Dataset.Processes
+
+import HEP.Automation.JobQueue.JobType
+
+processSetup :: ProcessSetup C8V
+processSetup = PS {  
+    model = C8V
+  , process = preDefProcess TTBar0or1J
+  , processBrief = "TTBar0or1J" 
+  , workname   = "710_C8V_TTBar0or1J_LHC"
+  }
+
+paramSet :: [ModelParam C8V]
+paramSet = [ C8VParam { mnp = m,  
+                        gnpR = g,
+                        gnpL = 0 } | m <- [200,400,600,800,1000]
+                                   , g <- [0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0] ]
+
+sets :: [Int]
+sets =  [1]
+
+ucut :: UserCut 
+ucut = UserCut { 
+    uc_metcut = 15.0
+  , uc_etacutlep = 2.7
+  , uc_etcutlep = 18.0 
+  , uc_etacutjet = 2.7
+  , uc_etcutjet = 15.0 
+}
+
+
+eventsets :: [EventSet]
+eventsets =  
+  [ EventSet  processSetup 
+              (RS { param = p
+                  , numevent = 10000
+                  , machine = LHC7 ATLAS 
+                  , rgrun   = Fixed
+                  , rgscale = 200.0
+                  , match   = MLM
+                  , cut     = DefCut 
+                  , pythia  = RunPYTHIA
+                  , usercut = NoUserCutDef -- UserCutDef ucut 
+                  , pgs     = RunPGS
+                  , jetalgo = AntiKTJet 0.4
+                  , uploadhep = NoUploadHEP
+                  , setnum  = num 
+                  })
+   | p <- paramSet , num <- sets     ]
+
+webdavdir :: WebDAVRemoteDir
+webdavdir = WebDAVRemoteDir "paper3/ttbar_LHC_c8v_scan"
